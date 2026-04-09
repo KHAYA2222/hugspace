5877afdcede4e7034d45e3c66952bf5a814cf863
KHAYA2222 <50751242+KHAYA2222@users.noreply.github.com>
2026-04-09 21:17:11 +0200

    kts redone

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 android {
     namespace = "com.khayelihle.hugspace"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "26.1.10909125"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
